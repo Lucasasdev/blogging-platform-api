0bd34214d094a5949410e7890122df5718b62ea4
Lucas Alves <Lucasasdev@gmail.com>
2024-05-19 11:31:23 -0300

    feat: setup server and application

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello, Node!");
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send(error.message);
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello, Node!");
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = parseInt(`${process.env.PORT}`);
+
+import app from "./app";
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
